f58995a97002599cbbf6632059ea28bd6bb6288d
somtougeh <smugeh@gmail.com>
2018-05-24 13:07:30 +0100

    Finishing up

--- js/app.js
+++ js/app.js
@@ -1,22 +1,8 @@
 var header = document.querySelector(".header_holder");
-var sticky = header.offsetTop;
-var nav_list = document.querySelectorAll(".nav_default");
 
-// Back to top function
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
-  // scrollFunction();
   headerDown();
-  // linkChange();
 };
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("myBtn").style.display = "block";
-//   } else {
-//     document.getElementById("myBtn").style.display = "none";
-//   }
-// }
 
 function headerDown() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
@@ -24,10 +10,4 @@ function headerDown() {
   } else {
     header.classList.remove("header_down");
   }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
 }
